69f7dfa50de0e3c8d0c374445dc5fca7b494471b
hiro <dubianda8@gmail.com>
2025-07-25 14:09:52 +0900

    Add validation error tests for tasks

--- backend/app/controllers/tasks_controller.rb
+++ backend/app/controllers/tasks_controller.rb
@@ -17,7 +17,7 @@ class TasksController < ApplicationController
       if @task.save
         render json: @task, status: :created
       else
-        render json: @task.errors, status: :unprocessable_entity
+        render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
       end
     end
   
