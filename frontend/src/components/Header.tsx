4b1a3de7fdd043193b61ae7abc66d57b1480f0d6
hiro <dubianda8@gmail.com>
2025-08-05 19:39:21 +0900

    Add Header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="bg-blue-600 text-white p-4">
+            <h1 className="text-lg font-bold">現場タスク管理アプリ</h1>
+        </header>
+    )
+}
+
+export default Header
